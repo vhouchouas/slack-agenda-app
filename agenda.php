vhouchouas/slack-agenda-app

commit 16ae17abbc47adb985200e0412c4e12e80ec4bc9

All registrations may create a reminder
This fixes #100.
The issue was that when a user register to an event which is displayed on
Slack after calling /remindEvent, then the registration is runs
`SlackEvents.more_inchannel` which doesn't call `Agenda.addReminder`.

This commit moves the creation of the reminder inside `Agenda.updateAttendee`
so every registration leads to the attempt to create a reminder.

agenda.php
@@ -495,6 +495,7 @@ WHERE vCalendarFilename =:vCalendarFilename;");
      * @param string $usermail the user email
      * @param boolean $register true: register, false: unregister
      * @param string $attendee_CN the attendee commun name
+     * @param string $userid the Slack id of the user
      *
      * @return boolean|null
      *    return null if the attendee is already registered and $register === true (i.e. nothing to do);
@@ -502,7 +503,7 @@ WHERE vCalendarFilename =:vCalendarFilename;");
      *    return true if no error occured;
      *    return false if the event has not been updated on the CalDAV server (i.e. the registration has failed).
      */    
-    public function updateAttendee(string $vCalendarFilename, string $usermail, bool $register, ?string $attendee_CN) {
+    public function updateAttendee(string $vCalendarFilename, string $usermail, bool $register, ?string $attendee_CN, string $userid) {
         $this->log->info("updating $vCalendarFilename");
         list($vCalendar, $ETag) = $this->getEvent($vCalendarFilename);
 
@@ -554,22 +555,33 @@ WHERE vCalendarFilename =:vCalendarFilename;");
         if($new_ETag === false) {
             $this->log->error("Fails to update the event");
             return false; // the event has not been updated
-        } else if(is_null($new_ETag)) {
-            $this->log->info("The CalDAV server did not answer a new ETag after an event update, need to update the local calendar");
-            $this->updateEvents(array($vCalendarFilename));
-            return true;// the event has been updated
         } else {
-            $this->log->info("The CalDAV server did answer a new ETag after an event update, no need to update the local calendar");
-            $event = array(
-                "vCalendarFilename" => $vCalendarFilename,
-                "vCalendarRaw" => $vCalendar->serialize(),
-                "ETag" => trim($new_ETag, '"')
-            );
-            $this->updateEvent($event);
+            if ($register) {
+                $eventStartDate = $vCalendar->VEVENT->DTSTART->getDateTime();
+                $reminderDate = $eventStartDate->modify("-1 day");
+                $summary = (string)$vCalendar->SUMMARY;
+                $this->addReminder($userid, $vCalendarFilename, $summary, $reminderDate);
+            } else {
+                $this->deleteReminder($userid, $vCalendarFilename);
+            }
+
+            if(is_null($new_ETag)) {
+                $this->log->info("The CalDAV server did not answer a new ETag after an event update, need to update the local calendar");
+                $this->updateEvents(array($vCalendarFilename));
+                return true;// the event has been updated
+            } else {
+                $this->log->info("The CalDAV server did answer a new ETag after an event update, no need to update the local calendar");
+                $event = array(
+                    "vCalendarFilename" => $vCalendarFilename,
+                    "vCalendarRaw" => $vCalendar->serialize(),
+                    "ETag" => trim($new_ETag, '"')
+                );
+                $this->updateEvent($event);
+                return true;
+            }
         }
-        return true;
     }
-    
+
     private function updateEvents(array $vCalendarFilenames) {
         $events = $this->caldav_client->fetchEvents($vCalendarFilenames);
         
@@ -584,7 +596,7 @@ WHERE vCalendarFilename =:vCalendarFilename;");
         return true;
     }
     
-    public function addReminder(string $userid, string $vCalendarFilename, string $message, DateTimeImmutable $datetime) {
+    private function addReminder(string $userid, string $vCalendarFilename, string $message, DateTimeImmutable $datetime) {
         $now = new DateTimeImmutable();
         if ($datetime < $now){
             $this->log->debug("not creating the reminder for $userid because " . $datetime->format('Y-m-dTH:i:s') . " is in the past");
@@ -605,7 +617,7 @@ WHERE vCalendarFilename =:vCalendarFilename;");
         }
     }
 
-    public function deleteReminder(string $userid, string $vCalendarFilename) {
+    private function deleteReminder(string $userid, string $vCalendarFilename) {
         $query = $this->pdo->prepare("SELECT id FROM {$this->table_prefix}reminders WHERE vCalendarFilename= :vCalendarFilename AND userid = :userid;");
         $query->execute(array(
             'vCalendarFilename' =>  $vCalendarFilename,

slackEvents.php
@@ -286,7 +286,8 @@ class SlackEvents {
             $response = $this->agenda->updateAttendee($vCalendarFilename,
                                                       $profile->email,
                                                       $register,
-                                                      getUserNameFromSlackProfile($profile));
+                                                      getUserNameFromSlackProfile($profile),
+                                                      $userid);
         }
     }
     
@@ -335,7 +336,7 @@ class SlackEvents {
         slackEvents::ack();
         $this->register_fast_rendering($vCalendarFilename, $userid, $profile->email, $register, $request, $parsed_event);
         
-        $response = $this->agenda->updateAttendee($vCalendarFilename, $profile->email, $register, getUserNameFromSlackProfile($profile));
+        $response = $this->agenda->updateAttendee($vCalendarFilename, $profile->email, $register, getUserNameFromSlackProfile($profile), $userid);
 
         if(is_null($response)) { //nothing to do
             return;
@@ -344,19 +345,8 @@ class SlackEvents {
         if($response === false) {
             trigger_error("Event update failed", E_USER_ERROR); // it will: call error_handler, inform user and exit.
         }
-
-        $vCalendar = $parsed_event['vCalendar']->VEVENT;
-        $datetime = $vCalendar->DTSTART->getDateTime();
-        $datetime = $datetime->modify("-1 day");
-        
-        if($register) {
-            $summary = (string)$vCalendar->SUMMARY;
-            $this->agenda->AddReminder($userid, $vCalendarFilename, $summary, $datetime);
-        } else {
-            $this->agenda->DeleteReminder($userid, $vCalendarFilename);
-        }
     }
-    
+
     function filters_has_changed($action, $userid) {
         $filters_to_apply = array();
         foreach($action->selected_options as $filter) {

tests/unitTests/AgendaTest.php
@@ -426,7 +426,7 @@ final class AgendaTest extends TestCase {
 
         // Act & Assert
         // // Assert that the function considers it was successfully executed
-        $this->assertTrue($sut->updateAttendee($event->id(), "you@gmail.com", true, "Your Name"));
+        $this->assertTrue($sut->updateAttendee($event->id(), "you@gmail.com", true, "Your Name", "You"));
 
         // // Assert that the remote caldav server has been updated with correct parameters
         $this->assertEquals($event->id(), $caldav_client->updatedEvents[0][0]);
@@ -443,7 +443,7 @@ final class AgendaTest extends TestCase {
 
         // Act & Assert
         // // Assert that the function noticed that nothing was done
-        $this->isNull($sut->updateAttendee($event->id(), "you@gmail.com", true, "Your Name"));
+        $this->isNull($sut->updateAttendee($event->id(), "you@gmail.com", true, "Your Name", "You"));
 
         // // Assert that we did not try to update the caldav server
         $this->assertEquals(0, count($caldav_client->updatedEvents));
@@ -461,7 +461,7 @@ final class AgendaTest extends TestCase {
 
         // Act & Assert
         // // Assert that the function considers it was successfully executed
-        $this->assertTrue($sut->updateAttendee($event->id(), "you@gmail.com", false, "Your Name"));
+        $this->assertTrue($sut->updateAttendee($event->id(), "you@gmail.com", false, "Your Name", "You"));
 
         // // Assert that the remote caldav server has been updated with correct parameters
         $this->assertEquals($event->id(), $caldav_client->updatedEvents[0][0]);
@@ -478,7 +478,7 @@ final class AgendaTest extends TestCase {
 
         // Act & Assert
         // // Assert that the function noticed that nothing was done
-        $this->isNull($sut->updateAttendee($event->id(), "you@gmail.com", false, "Your Name"));
+        $this->isNull($sut->updateAttendee($event->id(), "you@gmail.com", false, "Your Name", "You"));
 
         // // Assert that we did not try to update the caldav server
         $this->assertEquals(0, count($caldav_client->updatedEvents));

commit 94d6ae60bc54818e01b4247aad2f45ec3268ce53

Fix bug in reminder creation
because of a mistake, the summary in the reminder was empty

It is a regression introduced by 16ae17abbc47adb985200e0412c4e12e80ec4bc9
(because the line
    (string)$vCalendar->SUMMARY;
was moved but in the 1st context we had $vCalendar which was set to
    $vCalendar = $parsed_event['vCalendar']->VEVENT;
whereas in the other it was not)

agenda.php
@@ -696,7 +696,7 @@ WHERE vCalendarFilename =:vCalendarFilename;");
             if ($register) {
                 $eventStartDate = $vCalendar->VEVENT->DTSTART->getDateTime();
                 $reminderDate = $eventStartDate->modify("-1 day");
-                $summary = (string)$vCalendar->SUMMARY;
+                $summary = (string)$vCalendar->VEVENT->SUMMARY;
                 $this->addReminder($userid, $vCalendarFilename, $summary, $reminderDate);
             } else {
                 $this->deleteReminder($userid, $vCalendarFilename);